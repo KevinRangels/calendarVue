b72c20ee25629cbb6a3a46abc2d14729fb4b5f2a
Kevin Rangel <krangel107@gmail.com>
2020-01-17 23:26:43 -0430

    add loader, work with Vuex and async await

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,68 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<v-layout :wrap="true">
+		<v-flex xs12>
+			<v-card>
+				<v-date-picker color="gold"
+							   v-model="fecha"
+							   full-width
+							   locale="es-cl"
+							   :min="minimo"
+							   :max="maximo"
+							   @change="getDolar(fecha)"
+							   ></v-date-picker>
+			</v-card>
+			<v-card color="gold" gold>
+				<v-card-text  class="display-1 text-center" >
+					{{valor}} 
+				</v-card-text>
+			</v-card>
+		</v-flex>
+	</v-layout>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import axios from 'axios';
+import { mapMutations } from "vuex";
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+ data(){
+ 	return{
+ 		fecha: new Date().toISOString().substr(0, 10),
+ 		minimo: '1984',
+ 		maximo: new Date().toISOString().substr(0, 10),
+ 		valor: null
+ 	}
+ },
+ methods:{
+ 	...mapMutations(['mostrarLoading', 'ocultarLoading']),
+ 	async getDolar(dia){
+ 		let arrayFecha = dia.split(['-']) //Separa la fecha 
+
+ 		let ddmmyy = arrayFecha[2]+'-'+arrayFecha[1]+'-'+arrayFecha[0]; //se ordena la fecha
+ 		console.log(ddmmyy)
+ 		
+ 		try {
+
+ 			this.mostrarLoading({ titulo: 'Accediendo a informacion', color: 'gold'})
+ 			let datos = await axios.get(`https://mindicador.cl/api/dolar/${ddmmyy}`)
+ 			if(datos.data.serie.length > 0){
+ 				this.valor = await datos.data.serie[0].valor
+ 			}else{
+ 				this.valor = 'Sin resultados'
+ 			}
+
+ 		}catch (error) {
+ 			// console.log(error)
+ 		}
+ 		finally{
+ 			this.ocultarLoading()
+ 		}
+ 	
+ },
+ created(){
+ 	this.getDolar(this.fecha)
+ }
+}
 }
 </script>
